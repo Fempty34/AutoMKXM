37d83e8acabd82a9132375cf0156b25110d06516
Fempty34 <fempty@protonmail.com>
2024-04-05 10:58:04 +0300

    Add screen of hidden window

--- view.py
+++ view.py
@@ -1,14 +1,23 @@
+import os
 from time import sleep
 import cv2
 import numpy as np
 from mss import mss
 
+import win32gui
+import win32ui
+from ctypes import windll
+from PIL import Image
+
 
 def start_video(pos):
-    sct = mss()
+    #sct = mss()
     while True:
         sleep(0.5)
-        sct_img = sct.grab(pos)
+        #sct_img = sct.grab(pos)
+
+        sct_img = get_screen()
+
         cv2.imshow('screen', np.array(sct_img))
         menu = cv2.imread('src/images/menu.png')
         compare_img = cv2.cvtColor(np.array(sct_img), cv2.COLOR_BGR2RGB)
@@ -20,3 +29,42 @@ def start_video(pos):
             break
         if (cv2.waitKey(1) & 0xFF) == ord('s'):
             cv2.imwrite('src/images/daily_reward.png', np.array(sct_img))
+
+
+def get_screen():
+    window_name = os.getenv('MKWINDOWNAME')
+    hwnd = win32gui.FindWindow(None, window_name)
+
+    left, top, right, bot = win32gui.GetWindowRect(hwnd)
+    w = right - left
+    h = bot - top
+
+    hwndDC = win32gui.GetWindowDC(hwnd)
+    mfcDC = win32ui.CreateDCFromHandle(hwndDC)
+    saveDC = mfcDC.CreateCompatibleDC()
+
+    saveBitMap = win32ui.CreateBitmap()
+    saveBitMap.CreateCompatibleBitmap(mfcDC, w, h)
+
+    saveDC.SelectObject(saveBitMap)
+
+    result = windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 0)
+
+    bmpinfo = saveBitMap.GetInfo()
+    bmpstr = saveBitMap.GetBitmapBits(True)
+
+    im = Image.frombuffer(
+        'RGB',
+        (bmpinfo['bmWidth'], bmpinfo['bmHeight']),
+        bmpstr, 'raw', 'BGRX', 0, 1)
+
+    win32gui.DeleteObject(saveBitMap.GetHandle())
+    saveDC.DeleteDC()
+    mfcDC.DeleteDC()
+    win32gui.ReleaseDC(hwnd, hwndDC)
+
+    if result == 1:
+        # PrintWindow Succeeded
+        return im
+    else:
+        return -1
